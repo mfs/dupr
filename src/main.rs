c175953e32b99c9b15eaccbbbf04afa934447633
Mike Sampson <mike@sambodata.com>
2017-10-08 14:50:56 +1100

    Walk dir and display file name and size

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate clap;
+extern crate walkdir;
+
+use clap::{App, Arg};
+use walkdir::WalkDir;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("dupr")
+        .version("0.1.0")
+        .author("Mike Sampson")
+        .about("Duplicate file finder")
+        .arg(
+            Arg::with_name("DIR")
+                .help("Directory to process")
+                .required(true),
+        )
+        .get_matches();
+
+
+    for entry in WalkDir::new(matches.value_of("DIR").unwrap()) {
+        let entry = entry.unwrap();
+        let metadata = entry.metadata().unwrap();
+        println!("{} - {}", entry.path().display(), metadata.len());
+    }
 }
