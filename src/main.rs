a6c97d3ff9457688ffd0a067bd652e22398143ed
Mike Sampson <mike@sambodata.com>
2017-10-10 00:06:16 +1100

    Add hashing.
    For now just printing the hash of each file.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 extern crate clap;
 extern crate walkdir;
+extern crate twox_hash;
 
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
 
 use clap::{App, Arg};
 use walkdir::WalkDir;
+use twox_hash::XxHash;
+use std::hash::Hasher;
 
 fn main() {
     let matches = App::new("dupr")
@@ -22,6 +28,11 @@ fn main() {
 
     for entry in WalkDir::new(matches.value_of("DIR").unwrap()) {
         let entry = entry.unwrap();
+
+        if !entry.file_type().is_file() {
+            continue;
+        }
+
         let metadata = entry.metadata().unwrap();
 
         let path = entry.path().to_path_buf();
@@ -32,7 +43,25 @@ fn main() {
     for (f_len, f_paths) in files {
         println!("=== {} - {} ===", f_len, f_paths.len());
         for f_path in f_paths {
-            println!("    {}", f_path.display());
+            println!("    {} - {:x}", f_path.display(), hash_file(&f_path));
         }
     }
 }
+
+fn hash_file<P: AsRef<Path>>(path: P) -> u64 {
+    let mut file = File::open(path).unwrap();
+
+    let mut hash = XxHash::with_seed(0);
+
+    let mut buffer = [0; 32 * 1024];
+
+    while let Ok(n) = file.read(&mut buffer) {
+        if n == 0 {
+            break;
+        }
+        hash.write(&buffer[0 .. n]);
+    }
+
+    hash.finish()
+}
+
